1cc9ba97898bc8c6c40336e7284ce6290b854c09
JonasVeenstra <j.c.veenstra@uva.nl>
2024-02-10 16:08:59 +0100

    Added manual detection in loop. removed different modes

--- extract_circles.py
+++ extract_circles.py
@@ -1,64 +1,26 @@
 import numpy as np
-from tracking_class import Tracking
 import matplotlib.pyplot as plt
 from tracking_class import Tracking
 
 # this script detects and records the position/radius of circles in video format data
 
-# root = '/Users/Jonas'
-# path = '/Library/CloudStorage/OneDrive-UvA/data/Topological defects/experiment/Melanie_N=3/movies'
-# path = '/Downloads'
-# name  ='/tm_obstacle_low'
-# ext = '.mov'
-# 
-# root = '/Users/Jonas/Library/CloudStorage/OneDrive-UvA/data/limit cycles turn active matter/fatboy6'
-# path = '/cycles v2'
-# name  ='/passive'
-# ext = '.mov'
-
 root = '/Users/Jonas/Documents/LocalData'
-path = '/dec2023'
-name  ='/test3'
-ext = '.avi'
-
-
+path = '/feb2024'
+name  ='/eps=-0.01'
+ext = '.mov'
 
 filepath = [root,path,name,ext]
 
 
-
-def window(x0,x1,y0,y1):
-    # ROI window with x,y coords of rectangle in pixels.
-    ROI = {'window': np.asarray([x0,x1,y0,y1])}
-    return ROI
-    
-def coords(l):
-    # x,y coords of individual windows 
-    ROI = {'coords': np.asarray(l)}
-    return ROI
-
-# indicate ROI window (pixels):
-x_min = 0
-x_max = 9000
-y_min = 0
-y_max = 10100
-
-ROI = window(y_min,y_max,x_min,x_max)
-# alternatively, indicate coordinates for each particle:
-# ROI = coords(np.asarray([[400,880],[760,900]])) 
-
-#or interactively
-ROI = {'interactive':True}
 params = {'p1':100,
-          'p2': 24, #tune this parameter for detection threshold
-          'Nframe':0, #amount of frames towindow()
+          'p2': 21, #tune this parameter for detection threshold
+          'MinDist':20, #minimum distance between particles (only on frame 0)
+        #   'Nframe':0, #amount of frames towindow()
           'check':True,'overwrite':False,'save':True, 
-          'r_ROI':50, # ROI window width/height (pixels)
+          'r_ROI':25, # ROI window width/height (pixels)
           'filepath':filepath, 
-          'r_obj':[15,20], #expected circle radius 
-          'ROI':ROI,
-          'window':[x_min,x_max,y_min,y_max],
-          't0':0,'tf':1200}
+          'r_obj':[5,8], #expected circle radius 
+          't0':0,'tf':12000}
 
 f = Tracking(filepath = filepath)
 f.set_parameters(params)
--- tracking_class.py
+++ tracking_class.py
@@ -9,7 +9,7 @@ from turtle import distance
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+import sys,time
 import pickle
 import os
 from scipy.spatial import distance_matrix
@@ -38,44 +38,39 @@ class Tracking(object):
         self.cap.set(cv2.CAP_PROP_POS_FRAMES,self.f)
         self.totalframes =  int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
         self.totalframes = self.totalframes - self.f
+        
         if self.totalframes == -9223372036854775808:
             self.totalframes = 1
+
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
         if (self.cap.isOpened()== False):               
             print("Error opening video stream or file")
-        if self.params['Nframe'] == 0:
-            self.params['Nframe'] = self.totalframes
-        print('total frames: ',self.params['Nframe'])
+
+        print('TOTAL FRAMES: ',self.totalframes, f't0 = {self.params["t0"]}')
         
-        self.params['Nframe'] = min(self.totalframes,self.params['Nframe'])
         self.read_frame()
         self.findcircles()
         self.N = len(self.circles)
         
-        self.data.raw = np.zeros((self.params['Nframe'],self.N,3)) # t * N * 3 array with x,y,r data of detected circles
+        self.data.raw = np.zeros((self.totalframes,self.N,3)) # t * N * 3 array with x,y,r data of detected circles
         self.circles = np.asarray(self.circles)
-        
         print('circle radius = ' + str(np.mean(self.circles[:,2])))
         self.data.raw[0,:,:] = np.asarray(self.circles)
         self.f+=1
-        
         self.run_tracker()
 
     def run_tracker(self):
-        self.f=1
-        while(self.f<self.params['Nframe'] and self.f<(self.params['tf']-self.params['t0'])):
+        self.f=self.params['t0']+1
+        while(self.f<self.totalframes and self.f<(self.params['tf']-self.params['t0'])):
             print(self.f)
             if self.ret == False:
                 self.cap.release()
                 break
             self.read_frame()
-            self.params['ROI']= {'coords':self.data.raw[self.f-1,:,:2]}
             self.findcircles()
-            
             try:
                 self.data.raw[self.f,:,:] = np.asarray(self.circles)    
             except ValueError:
-                # code something here to deal with nonconservative particle numbers
                 print('particle number not conserved')
                 break
             self.f+=1  
@@ -89,107 +84,126 @@ class Tracking(object):
         self.timestamps.append(self.cap.get(cv2.CAP_PROP_POS_MSEC)/1e3)
         if self.ret==True:
             self.cimg = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        elif self.f ==self.params['Nframe']-1:
+        elif self.f ==self.totalframes-1:
             pass
         else:
             print('warning, frame not found')
-    
-    def findcircles(self):
-        def circlefitter(cimg):
-            p1 = self.params['p1']
-            p2 = self.params['p2']
-            r1,r2 = self.params['r_obj']
-            self.circles = cv2.HoughCircles(cimg,3,1,20,param1=p1,param2=p2,minRadius=r1,maxRadius=r2)[:][:][0]
+
+
+    def onclick(self,event):
+        global ix, iy
+        ix, iy = event.xdata, event.ydata
+        print (f'x = {ix}, y = {iy}')
+        self.clicked_coordinate.append([ix,iy])
+    def photocheck(self):
         
-        self.coords =[]
-        if 'interactive' in self.params['ROI']:
-            self.circles=np.array([[]])
-            fig,ax = self.photo_plot(a=1)
-            def onclick(event):
-                global ix, iy
-                ix, iy = event.xdata, event.ydata
-                print (f'x = {ix}, y = {iy}')
-                self.coords.append([ix,iy])
-
-            cid = fig.canvas.mpl_connect('button_press_event', onclick)            
+        
+        while(self.params['check']):
+            print(f'current N={len(self.circles)}')
+            self.clicked_coordinate=[]
+            fig,ax = self.photo_plot()
+            cid = fig.canvas.mpl_connect('button_press_event', self.onclick)            
             plt.show()
-            
-            self.params['ROI'] = {'coords': np.asarray(self.coords)}
+            if len(self.clicked_coordinate)==0: 
+                break
 
-        ROI = self.params['ROI']
-        if ROI is None:
-            circlefitter(self.cimg)
-        elif 'window' in ROI:
-            window = ROI['window']
-            self.xmin = window[0]
-            self.xmax = window[1]
-            self.ymin = window[2]
-            self.ymax = window[3]
-            
-            self.ROIimg = self.cimg[self.ymin:self.ymax,self.xmin:self.xmax]
-            circlefitter(self.ROIimg)
-            # plt.show()
-            for ni,i in enumerate(self.circles):
-                self.circles[ni][0] +=  self.xmin
-                self.circles[ni][1] +=  self.ymin
-            self.circles = self.circles[np.argsort(np.sqrt(self.circles[:, 0]**2+self.circles[:, 1]**2))]
-            print('photocheck')
-            self.photocheck(a='area')
-            
-        elif 'coords' in ROI:
-            coords = ROI['coords']
-            circles = []
-            for n,unit in enumerate(coords):   ####loop over all N ROI's
+
+            for n,unit in enumerate(self.clicked_coordinate):   ####loop over all N ROI's
                 self.unitnumber=n
                 y = int(unit[0])            ##### CAREFUL: image input is of form [y,x] (cf. '1080x1920')
                 x = int(unit[1])
                 ROIr = self.params['r_ROI']
-                self.xmin = np.max([x - ROIr, self.params['window'][0]])
-                self.xmax = np.min([np.max([x + ROIr,2*ROIr]), self.params['window'][3]])
-                self.ymin = np.max([y - ROIr, self.params['window'][2]])
-                self.ymax = np.min([np.max([y + ROIr,2*ROIr]), self.params['window'][1]])
+                self.xmin = np.max([x - ROIr])
+                self.xmax = np.min([np.max([x + ROIr,2*ROIr])])
+                self.ymin = np.max([y - ROIr])
+                self.ymax = np.min([np.max([y + ROIr,2*ROIr])])        
+                self.ROIimg = self.cimg[self.xmin:self.xmax,self.ymin:self.ymax]
+                
+                while True:
+                    added_circle = self.circlefitter(self.ROIimg)
+                    if len(added_circle)==0:
+                        self.params['p2']+=-1
+                    elif len(added_circle)==0:
+                        self.params['p2']+=1
+                    else: break
+                
+                added_circle[0][:2] +=  [self.ymin,self.xmin]#[y-int(ROIr),x-int(ROIr)]#HERE
+                added_circle=np.array(added_circle[0])
+                self.circles=np.append(self.circles,[added_circle],axis=0)
 
-        
+
+
+    def photo_plot(self):
+        fig,ax = plt.subplots(1,figsize=(6,6))
+        circles = np.asarray(self.circles)
+        ax.imshow(cv2.cvtColor(self.cimg,cv2.COLOR_GRAY2RGB))        
+        for n,i in enumerate(circles):
+            x,y=[i[0],i[1]]
+            ax.annotate(str(n),[x,y],c='white')
+            ax.add_patch(plt.Circle((x,y),i[2],color='r',lw = 1,fill=False))
+        return fig,ax
+    def circlefitter(self,cimg):
+        p1 = self.params['p1']
+        p2 = self.params['p2']
+        mindist = self.params['MinDist']
+        r1,r2 = self.params['r_obj']
+        try:
+            circles = cv2.HoughCircles(cimg,3,1,minDist=mindist,param1=p1,param2=p2,minRadius=r1,maxRadius=r2)[:][:][0]
+        except TypeError:
+            circles = []
+        return circles
+
+    def manual_detection(self):
+        self.clicked_coordinate=[]
+        fig,ax = plt.subplots(1,figsize=(6,6))
+        ax.imshow(cv2.cvtColor(self.ROIimg,cv2.COLOR_GRAY2RGB))        
+        cid = fig.canvas.mpl_connect('button_press_event', self.onclick) 
+        plt.show()           
+        self.clicked_coordinate=self.clicked_coordinate[0]
+        self.clicked_coordinate.append(0)
+        return np.array([self.clicked_coordinate])
+    def findcircles(self):
+        if self.f==self.params['t0']: #frame 0
+            self.ROIimg = self.cimg
+            self.circles = self.circlefitter(self.ROIimg)
+            self.photocheck()
+            time.sleep(0.5)
+            plt.close()
+
+
+        else: #rest of frames
+            coords = self.circles
+            circles = []
+            for n,xy in enumerate(coords):   ####loop over all N ROI's
+                self.unitnumber=n
+                y = int(xy[0])            ##### CAREFUL: image input is of form [y,x] (cf. '1080x1920')
+                x = int(xy[1])
+                ROIr = self.params['r_ROI']
+                self.xmin = np.max([x - ROIr])
+                self.xmax = np.min([np.max([x + ROIr,2*ROIr])])
+                self.ymin = np.max([y - ROIr])
+                self.ymax = np.min([np.max([y + ROIr,2*ROIr])])
                 self.ROIimg = self.cimg[self.xmin:self.xmax,self.ymin:self.ymax]
                 run = True
-                p=0
-                
+                i=0
                 while(run):
-                    try:
-                        circlefitter(self.ROIimg)
-                        circle = self.circles
-                        if len(circle) > 1:
-                            if self.params['p2'] == p:
-                                print(f'cannot find particle {n+1}. skipping frame...1')
-                                circles.append(self.data.raw[self.f - 1,n,:])
-                                self.circles=np.asarray(circles)
+                    circle =self.circlefitter(self.ROIimg)
+                    if i>3:
+                        circle = self.manual_detection()
+                        print(circle)
 
-                                fig,ax = self.photo_plot(a=0)
-                                plt.show()
-                                run = False
-                            else:
-                                print('Found more particles than expected: increasing threshold')
-                                p = self.params['p2']
-                                self.increase_threshold()
-                        else:
-                            circle[0][:2] +=  [self.ymin,self.xmin]#[y-int(ROIr),x-int(ROIr)]#HERE
-                            circles.append(list(circle[0]))
-                            run = False
-                    except TypeError:
-                        print('TYPEERROR')
+                    if len(circle) == 0:
+                        i+=1
                         self.decrease_threshold()
-                        if self.params['p2'] == p or self.params['p2']==0:
-                            print(f'cannot find particle {n+1}. skipping frame...2')
-                            circles.append(self.data.raw[self.f - 2,n,:])
-                            self.circles=np.asarray(circles)
-                            fig,ax = self.photo_plot(a=0)
-                            plt.show()
-                        else:
-                            print(f'particle {n+1} missing: decreasing threshold')
-                            p = self.params['p2']
+                    elif len(circle)>1:
+                        i+=1
+                        self.increase_threshold()
+                    elif len(circle)==1:
+                        circle[0,:2] +=  [self.ymin,self.xmin]
+                        circles.append(list(circle[0]))
+                        run = False
+
             self.circles = np.asarray(circles)
-            self.photocheck(a='area')
-            self.params['r_ROI']=15
 
     def increase_threshold(self):
         self.params['p2'] = self.params['p2'] + 1
@@ -198,55 +212,6 @@ class Tracking(object):
     def decrease_threshold(self):
         self.params['p2'] = self.params['p2'] - 1
         print('p2 is now:    ',self.params['p2'])
-
-    def photo_plot(self,a=0,):
-        fig,ax = plt.subplots(1,figsize=(12,6))
-        circles = np.asarray(self.circles)
-        if a==0:
-            self.xmin,self.xmax,self.ymin,self.ymax = [int(np.min(circles[:,0])),int(np.max(circles[:,0])),int(np.min(circles[:,1])),int(np.max(circles[:,1]))]
-        if 'window' in self.params['ROI']:
-            rnge = self.params['ROI']['window']
-            print(rnge)
-            self.cimg = self.cimg[rnge[0]:rnge[1],rnge[2]:rnge[3]]
-        print(1)
-        for n,i in enumerate(circles):
-            if 'window' in self.params['ROI']:
-                x=i[0]-rnge[2]
-                y=i[1]-rnge[0]
-                if not 'interactive' in self.params['ROI']:
-                    x,y=[i[0],i[1]]
-                    ax.annotate(str(n),[x,y],c='white')
-                    # ax.scatter(x,y,c='r',s=7)
-                    ax.add_patch(plt.Circle((x,y),i[2],color='r',lw = 1,fill=False))
-        
-        ax.imshow(cv2.cvtColor(self.cimg,cv2.COLOR_GRAY2RGB))        
-        return fig,ax
-
-    def photocheck(self,a=0):
-        while(self.params['check']):
-            fig,ax= self.photo_plot(a=a)
-            plt.show()
-            
-            N=len(self.circles)
-            
-            print(f'{N} circles detected. Increase (i) or decrease (d) threshold p2? otherwise ENTER. p2=',self.params['p2'])
-            print('average radius: ', np.average(np.array(self.circles)[:,2]))
-            yes = ''
-            inc = 'i'
-            dec = 'd'
-
-            choice = input().lower()
-            if choice == dec:
-                self.decrease_threshold()
-                self.findcircles()
-            elif choice == inc:
-                self.increase_threshold()
-                self.findcircles()
-            elif choice not in [yes,inc,dec]:
-                sys.stdout.write("'m' or 'f'")
-            elif choice == yes:
-                self.params['check'] = False
-        self.N = len(self.circles)
     
     def extract_data(self):
         if not os.path.exists(self.picklepath):
